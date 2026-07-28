b54e542589ae0316dd40c0639ace42a2e6c153b2
he116114379 <116114379@qq.com>
2026-07-28 15:42:34 +0800

    更新 Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val appVersion = "3.4.2.3"
     const val versionCode = 1500
 
-    const val compileSdk = 28
+    const val compileSdk = 34
     const val minSdk = 27
     const val targetSdk = 28
     const val wearMinSdk = 27
